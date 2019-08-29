80ff5ae5845cf9ba95fa0bf4461227bf8e962d41
unknown <nilakantha_deo@bll04814.persistent.co.in>
2019-08-29 19:42:55 +0530

    created rest API with django-tastypie

--- api/models.py
+++ api/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from testypie.resources import ModelResource
+from tastypie.resources import ModelResource
 from movies.models import Movie
 
 # Create your models here.
 class MovieResource(ModelResource):
     class Meta:
         queryset = Movie.objects.all()
-        resource_name=movies
+        resource_name='movies'
